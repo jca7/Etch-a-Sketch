4d4c7b77a64aa54c99fdd1a302b536efd2b82f1b
Jesse Chan <j.chan092@gmail.com>
2023-10-03 21:46:31 -0700

    Added button + eventListener for clicks on button

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () { // Adding rows to the container
-  const container = document.getElementById("container");
+  const topContainer = document.getElementById("container");
   for (let i = 0; i < 16; i++) {
-      const newDiv = document.createElement("div");
-      newDiv.classList.add("rows");
-      container.appendChild(newDiv);
+      const newRowDiv = document.createElement("div");
+      newRowDiv.classList.add("rows");
+      topContainer.appendChild(newRowDiv);
   }
   const rowContainer = document.querySelectorAll(".rows"); // selecting all the created rows
   for (let i = 0; i < rowContainer.length; i++) { // looping through EACH of the rows.
     const row = rowContainer[i];
     for (let j = 0; j < 16; j++) { // Creating squares for each of the 16 rows.  
-      const newSquare = document.createElement("div");
-      newSquare.classList.add("square");
-      row.appendChild(newSquare);
+      const newSquareDiv = document.createElement("div");
+      newSquareDiv.classList.add("square");
+      row.appendChild(newSquareDiv);
     }
   }
   const squares = document.querySelectorAll(".square"); // selecting all squares
@@ -21,7 +21,10 @@ document.addEventListener("DOMContentLoaded", function () { // Adding rows to th
 
 });
 
+document.addEventListener('click', askForNumber)
 
+function askForNumber() { 
+  let number = prompt("Change the number of squares to (Max - 100): ");
 
-
+}
 
